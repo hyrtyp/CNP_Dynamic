a6d8920adbe9db70fe28a13e30c953f388c75545
GYH <790005156>
2014-03-28 18:30:00 +0800

    new dynamicnew

--- src/main/java/com/hyrt/cnp/dynamic/fragment/AlldynamicFragment.java
+++ src/main/java/com/hyrt/cnp/dynamic/fragment/AlldynamicFragment.java
@@ -58,7 +58,11 @@ public class AlldynamicFragment extends Fragment {
         isFirst = false;
         initView(rootview);
         initData();
-        loadData();
+        if(dynamics.size()==0){
+            loadData();
+        }else{
+            allDataUi();
+        }
         return rootview;
     }
 
@@ -142,6 +146,22 @@ public class AlldynamicFragment extends Fragment {
             }
         }
         STATE = "";//清空状态
+    }
+
+    /**
+     * 将已有的数据放到listview中
+     * */
+    private void allDataUi(){
+        String[] resKeys = new String[]{"getUserphoto", "getUserName",
+                "getPosttime3", "getContent2",
+                "getsPicAry0", "getsPicAry1",
+                "getsPicAry2", "getPosttime2", "getTransmit2", "getReview2", "gettContent"};
+        int[] reses = new int[]{R.id.dynamic_Avatar, R.id.dynamic_name,
+                R.id.dynamic_time, R.id.dynamic_context,
+                R.id.dynamic_image1, R.id.dynamic_image2,
+                R.id.dynamic_image3, R.id.dynamic_time2, R.id.dynamic_zf_num, R.id.dynamic_pl_num, R.id.dynamic_dcontext};
+        dynamicAdapter = new DynamicAdapter(activity, dynamics, R.layout.layout_item_dynamic, resKeys, reses);
+        xListView.setAdapter(dynamicAdapter);
     }
 
     @Override
---
+++ src/main/java/com/hyrt/cnp/dynamic/ui/DynamicNewActivity.java
@@ -0,0 +1,10 @@
+package com.hyrt.cnp.dynamic.ui;
+
+import com.jingdong.common.frame.BaseActivity;
+
+/**
+ * Created by GYH on 2014/3/28.
+ */
+public class DynamicNewActivity extends BaseActivity{
+
+}
