3bc031d5843dfd5d100e2caba6d6b1db2bab416c
GYH <790005156>
2014-04-06 15:52:51 +0800

    no

---
+++ src/main/java/com/hyrt/cnp/dynamic/requestListener/MyAlbumRequestListener.java
@@ -0,0 +1,46 @@
+package com.hyrt.cnp.dynamic.requestListener;
+
+import android.app.Activity;
+
+import com.hyrt.cnp.base.account.requestListener.BaseRequestListener;
+import com.hyrt.cnp.dynamic.ui.HomeInteractiveActivity;
+import com.octo.android.robospice.persistence.exception.SpiceException;
+
+/**
+ * Created by GYH on 14-1-16. change
+ */
+public class MyAlbumRequestListener extends BaseRequestListener{
+    /**
+     * @param context
+     */
+    public MyAlbumRequestListener(Activity context) {
+        super(context);
+    }
+
+    @Override
+    public void onRequestFailure(SpiceException e) {
+//        showMessage(R.string.nodata_title,R.string.nodata_content);
+        super.onRequestFailure(e);
+        HomeInteractiveActivity activity = (HomeInteractiveActivity)context.get();
+        activity.upDataAblumUI(null);
+    }
+
+    @Override
+    public void onRequestSuccess(Object data) {
+        super.onRequestSuccess(data);
+        if(data!=null){
+        HomeInteractiveActivity activity = (HomeInteractiveActivity)context.get();
+        activity.upDataAblumUI(null);
+        }else{
+            HomeInteractiveActivity activity = (HomeInteractiveActivity)context.get();
+            activity.upDataAblumUI(null);
+        }
+
+    }
+
+    @Override
+    public MyAlbumRequestListener start() {
+        showIndeterminate("加载中...");
+        return this;
+    }
+}
